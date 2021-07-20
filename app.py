358064935e197a53199c6a2727c1e0d7e30fdd50
Narendra635 <narendra635@gmail.com>
2021-07-20 14:44:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,26 @@ import json
 import ast
 import re
 from copy import deepcopy
+from fastapi.middleware.cors import CORSMiddleware
 
 
 # 2. Create the app object
 app = FastAPI()
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 loaded_model = pickle.load(open( 'ClassificationModel.pkl', 'rb'))
 cv = pickle.load(open( 'transform.pkl', 'rb'))
